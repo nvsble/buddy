87eb3875a92fb64ed78b4f1c5bd3a038182b33a4
Young Seok Seo <young7seo@gmail.com>
2019-01-20 04:45:54 -0500

    added text-to-speech file (JS)

---
+++ src/components/features/tts.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const util = require('util');
+
+// Imports the Google Cloud client library
+const tts = require('@google-cloud/text-to-speech');
+
+// Creates a client
+const client = new tts.TextToSpeechClient();
+
+// The text to synthesize
+const text = 'Hello, world!';
+
+// Construct the request
+const request = {
+    input: {text: text},
+    // Select the language and SSML Voice Gender (optional)
+    voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+    // Select the type of audio encoding
+    audioConfig: {audioEncoding: 'MP3'},
+};
+
+// Performs the Text-to-Speech request
+const [response] = await client.synthesizeSpeech(request);
+// Write the binary audio content to a local file
+const writeFile = util.promisify(fs.writeFile);
+await writeFile('output.mp3', response.audioContent, 'binary');
+console.log('Audio content written to file: output.mp3');
